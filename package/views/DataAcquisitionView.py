972b041899de019d417a25bff2bb8d089d4f4da7
Pablo Losada <pablolr9@gmail.com>
2021-03-17 12:36:02 +0100

    started working on the camera

--- package/views/DataAcquisitionView.py
+++ package/views/DataAcquisitionView.py
@@ -1,11 +1,34 @@
 # This Python file uses the following encoding: utf-8
 import os
+import cv2
 
-from PySide2.QtWidgets import  QMainWindow
-from PySide2.QtCore import QFile
+from PySide2.QtCore import QEvent, QFile, QThread, Qt, Signal, Slot
+from PySide2.QtGui import QImage, QPixmap
 from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QMainWindow
 
 
+class CameraThreat(QThread):
+    update_frame = Signal(QImage)
+
+    def run(self):
+        self.running = True
+        cap = cv2.VideoCapture(0)
+        
+        while self.running:
+            ret, frame = cap.read()
+            if not ret: break
+            self.send_frame_to_ui(frame)
+
+    def send_frame_to_ui(self, frame):
+        rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgbImage.shape
+        bytesPerLine = ch * w
+        convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
+        p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+        self.update_frame.emit(p)
+
+        
 class DataAcquisitionView(QMainWindow):
 
     def __init__(self, model, controller):
@@ -17,12 +40,27 @@ class DataAcquisitionView(QMainWindow):
         self.connect_to_controller()
         self.connect_to_model()
         self.set_default_values()
-
+        
+        self.th = CameraThreat(self)
+        self.th.update_frame.connect(self.setImage)
+        self.window.installEventFilter(self)
+        
     def open(self):
         self.window.show()
+        self.th.start()
 
     def close(self):
         self.window.hide()
+        
+    def stop_threat(self):
+        self.th.running = False
+
+    def eventFilter(self, obj, event):
+        if obj is self.window and event.type() == QEvent.Close:
+            self.stop_threat()
+            event.accept()
+            return True
+        return super(DataAcquisitionView, self).eventFilter(obj, event)
 
     def load_ui(self):
         loader = QUiLoader()
@@ -40,3 +78,7 @@ class DataAcquisitionView(QMainWindow):
     
     def set_default_values(self):
         pass
+
+    @Slot(QImage)
+    def setImage(self, value):
+        self.window.cam_view.setPixmap(QPixmap.fromImage(value))
