e0b8efff825b5abcccdd2a1eb817cff6d9ec96d7
Pablo <plosada.rguez@gmail.com>
2021-05-24 16:48:33 +0200

    path testing

--- app/package/services/path.py
+++ app/package/services/path.py
@@ -89,9 +89,9 @@ def _trim_last_nans(array, verbose=False):
     i = 0
     le = len(array_rev)
     while i < le:
-        i += 1
         if not np.isnan(array_rev[i]):
             break
+        i += 1
 
     array_rev = array_rev[i:]
 
