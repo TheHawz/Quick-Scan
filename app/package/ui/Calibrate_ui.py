709b598e9642e2d161bbb37ea17838dcd25d19ba
Pablo Losada <pablolr9@gmail.com>
2021-05-30 11:03:28 +0200

    start adding calibration

---
+++ app/package/ui/Calibrate_ui.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'Calibrate.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(429, 596)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout = QVBoxLayout(self.frame)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.frame_2 = QFrame(self.frame)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame_2)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label = QLabel(self.frame_2)
+        self.label.setObjectName(u"label")
+
+        self.horizontalLayout_2.addWidget(self.label)
+
+        self.lineEdit = QLineEdit(self.frame_2)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.horizontalLayout_2.addWidget(self.lineEdit)
+
+
+        self.verticalLayout.addWidget(self.frame_2)
+
+        self.frame_3 = QFrame(self.frame)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setFrameShape(QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_3 = QHBoxLayout(self.frame_3)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.pushButton = QPushButton(self.frame_3)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.horizontalLayout_3.addWidget(self.pushButton)
+
+
+        self.verticalLayout.addWidget(self.frame_3)
+
+        self.label_2 = QLabel(self.frame)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setWordWrap(True)
+        self.label_2.setOpenExternalLinks(True)
+
+        self.verticalLayout.addWidget(self.label_2)
+
+        self.label_3 = QLabel(self.frame)
+        self.label_3.setObjectName(u"label_3")
+
+        self.verticalLayout.addWidget(self.label_3)
+
+        self.label_4 = QLabel(self.frame)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setScaledContents(False)
+        self.label_4.setWordWrap(True)
+
+        self.verticalLayout.addWidget(self.label_4)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
+
+
+        self.horizontalLayout.addWidget(self.frame)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 429, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u" -> Sonometer medition", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Calibrate!", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"The user will have to recalibrate the system each time any of the following happen:", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u" - The user change the input gain of the system.", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u" - The user change the microphone, the audio interface or any of the components that form the recording chain.", None))
+    # retranslateUi
+
---
+++ app/package/views/Calibrate_view.py
@@ -0,0 +1,153 @@
+# This Python file uses the following encoding: utf-8
+import os
+
+from PySide2.QtWidgets import QMainWindow, QFileDialog
+from PySide2.QtCore import Slot
+
+from ..ui.NewProject_ui import Ui_MainWindow as NewProject_ui
+
+
+class CalibrateView(QMainWindow, Calibrate_ui):
+
+    def __init__(self, model, controller):
+        super(NewProjectView, self).__init__()
+        self._model = model
+        self._controller = controller
+
+        self.setupUi(self)
+        self.connect_to_controller()
+        self.connect_to_model()
+        self.set_default_values()
+
+    def open(self):
+        self.show()
+
+    def close(self):
+        self.hide()
+
+    def connect_to_controller(self):
+        self.line_project_name.textChanged.connect(
+            self._controller.change_project_name)
+        self.line_project_location.textChanged.connect(
+            self._controller.change_project_location)
+        self.open_location.clicked.connect(self.open_location_dialog)
+        self.cb_audio_driver.currentIndexChanged.connect(
+            self._controller.change_audio_driver)
+        self.cb_audio_device.currentIndexChanged.connect(
+            self._controller.change_audio_device)
+        self.but_create.clicked.connect(
+            self._controller.create_new_project)
+        self.cb_video_devices.currentIndexChanged.connect(
+            self._controller.change_video_device)
+        self.low_freq_dial.valueChanged.connect(
+            self._controller.change_low_freq)
+        self.high_freq_dial.valueChanged.connect(
+            self._controller.change_high_freq)
+        self.open_project_button.clicked.connect(self.open_project)
+
+    def connect_to_model(self):
+        self._model.project_name_changed.connect(self.project_name_changed)
+        self._model.project_location_changed.connect(
+            self.handle_project_location_changed)
+        self._model.audio_drivers_set.connect(self.handle_audio_drivers_set)
+        self._model.audio_driver_changed.connect(
+            self.handle_audio_driver_changed)
+        self._model.audio_devices_set.connect(self.handle_audio_devices_set)
+        self._model.audio_device_changed.connect(
+            self.handle_audio_device_changed)
+        self._model.video_devices_set.connect(self.handle_video_devices_set)
+        self._model.video_device_changed.connect(
+            self.handle_video_device_changed)
+        self._model.low_freq_changed.connect(self.handle_low_freq_changed)
+        self._model.high_freq_changed.connect(self.handle_high_freq_changed)
+        self._model.low_freq_forced.connect(self.handle_low_freq_forced)
+        self._model.high_freq_forced.connect(self.handle_high_freq_forced)
+        self._model.minimum_time_changed.connect(
+            self.handle_minimum_time_changed)
+
+    def set_default_values(self):
+        self._controller.change_project_location(os.path.expanduser("~"))
+        self._controller.change_project_name('New Project')
+
+        self._controller.get_audio_drivers()
+        self._controller.set_video_devices()
+        self.high_freq_dial.setValue(1000)
+        self.low_freq_dial.setValue(40)
+        hostapi, device = self._controller.get_default_audio_device()
+        self.cb_audio_driver.setCurrentIndex(hostapi)
+        self.cb_audio_device.setCurrentIndex(device)
+
+    def open_location_dialog(self):
+        _dir = str(QFileDialog.getExistingDirectory(
+            self, "Choose a location.", str(self._model.project_location)))
+        if _dir != '':
+            self._controller.change_project_location(_dir)
+
+    @Slot()
+    def open_project(self):
+        fpath, _ = QFileDialog.getOpenFileName(self, "Open a project.", str(
+            self._model.project_location), filter="Project files (*.pro)")
+        if fpath != '':
+            self._controller.load_new_project(fpath)
+
+    # region SLOTS
+
+    @Slot(str)
+    def project_name_changed(self, value):
+        self.line_project_name.setText(value)
+
+    @Slot(str)
+    def handle_project_location_changed(self, value):
+        self.line_project_location.setText(value)
+
+    @Slot(object)
+    def handle_audio_drivers_set(self, value):
+        # driver_names = [*value]  # Unpacking dict keys
+        names = [v['name'] for v in value]
+        self.cb_audio_driver.clear()
+        self.cb_audio_driver.addItems(names)
+
+    @Slot(int)
+    def handle_audio_driver_changed(self, value):
+        self._controller.set_audio_devices(value)
+
+    @Slot(object)
+    def handle_audio_devices_set(self, value):
+        self.cb_audio_device.clear()
+        self.cb_audio_device.addItems(value)
+
+    @Slot(int)
+    def handle_audio_device_changed(self, value):
+        print(f'[VIEW]: Audio Device changed: {value}')
+
+    @Slot(object)
+    def handle_video_devices_set(self, value):
+        self.cb_video_devices.clear()
+        self.cb_video_devices.addItems(list(value.values()))
+
+    @Slot(int)
+    def handle_video_device_changed(self, value):
+        print(f'[VIEW]: Video Device changed: {value}')
+
+    @Slot(int)
+    def handle_low_freq_changed(self, value):
+        self.low_freq_label.setText(freq_to_text(value))
+
+    @Slot(int)
+    def handle_high_freq_changed(self, value):
+        self.high_freq_label.setText(freq_to_text(value))
+
+    @Slot(int)
+    def handle_low_freq_forced(self, value):
+        self.low_freq_dial.setValue(math.floor(math.log(value))*10-1)
+
+    @Slot(int)
+    def handle_high_freq_forced(self, value):
+        self.high_freq_dial.setValue(math.ceil(math.log(value))*10+1)
+
+    @Slot(float)
+    def handle_minimum_time_changed(self, value):
+        self.minimum_time_label.setText(
+            f'Minimum time of recording:\n {round(value, 2)}s')
+        ActualProjectModel.time_of_rec = value
+    # endregion
