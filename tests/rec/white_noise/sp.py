2d32d22940f120384eac7d88db250e7563ea55ba
Pablo Losada Rodriguez <plosada.rguez@gmail.com>
2021-06-01 14:48:04 +0200

    added script for spectrum

--- tests/rec/white_noise/sp.py
+++ tests/rec/white_noise/sp.py
@@ -1,15 +1,14 @@
 # %%
 
 # from scipy.fft import fft, fftshift
-from scipy.fft import fft, fftshift
 import numpy as np
 from scipy import signal
 from scipy.io.wavfile import read
-from scipy.fft import fftshift
+from scipy.fft import rfft, rfftfreq
 import matplotlib.pyplot as plt
 
-files = ['frontal/1622536219.0725663sin-bola.wav',
-         'frontal/1622536047.389629con-bola.wav']
+files = ['tests/rec/white_noise/frontal/1622536219.0725663sin-bola.wav',
+         'tests/rec/white_noise/frontal/1622536047.389629con-bola.wav']
 
 fs, x = read(files[1])
 x = np.transpose(x)
@@ -36,45 +35,46 @@ def get_num_of_windows(len_audio, win_size, overlap):
 
 
 # %%
-win_type = 'square'
-win_size = 2**15
-overlap = 0.1
-len_audio = x.shape[0]
-fraction = 3
+def get_spectrum(win_size=2**8):
+    win_type = 'hanning'
+    overlap = 0.1
+    len_audio = x.shape[0]
+    fraction = 3
 
-i = 0
-index = 0
+    i = 0
+    index = 0
 
-num_of_win = get_num_of_windows(len_audio, win_size, overlap)
-window = signal.windows.hann(2**14)
-# spls = np.zeros([num_of_win, len(x)])
-spls = []
+    window = signal.windows.hann(win_size)
+    spls = []
 
-# print(f'len_audio = {len_audio}')
-# print(f'num_of_win = {num_of_win}')
+    while i+win_size < len_audio:
+        # Windowing the signal.
+        start = i
+        end = i+win_size
+        x_w = x[start:end]
+        if win_type != 'square':
+            x_w *= window
 
-while i+win_size < len_audio:
-    # if index % 50 == 0:
-    #     print(f'Progres: {index} / {num_of_win}...')
+        # Calculating the spectrum (rfft)
+        yf = rfft(x_w)
+        yf = 20*np.log10(abs(yf))
+        xf = rfftfreq(win_size, 1 / fs)
+        spls.append(yf)
 
-    start = i
-    end = i+win_size
+        # Updating index
+        index += 1
+        i += round(win_size-win_size*overlap)
 
-    x_w = x[start:end]
-    if win_type != 'square':
-        x_w *= window
+    # Apply mean to all of the SPLs
+    spl = np.mean(spls, 0)
 
-    freq = [1, 1, 1]
-    _spl = [0, 0, 0]
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
 
-    spls.append(_spl)
+    ax.plot(xf, spl)
+    ax.set_xscale('log')
+    ax.set_xlim([20, 20e3])
+    plt.show()
 
-    index += 1
-    i += round(win_size-win_size*overlap)
 
-
-spl = np.mean(spls, 0)
-
-fig = plt.Figure()
-ax = fig.subplot()
-ax.plot(freq, spl)
+get_spectrum(win_size=2**10)
