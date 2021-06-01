43c2f80721494f6f33092d84560b5971c62657e5
Pablo <pablolr9@gmail.com>
2021-06-01 18:03:55 +0200

    results freq analysis

--- tests/rec/white_noise/sp.py
+++ tests/rec/white_noise/sp.py
@@ -1,28 +1,40 @@
 # %%
-
-# from scipy.fft import fft, fftshift
+from scipy.ndimage.filters import uniform_filter1d
 import numpy as np
 from scipy import signal
 from scipy.io.wavfile import read
 from scipy.fft import rfft, rfftfreq
 import matplotlib.pyplot as plt
 
-files = ['tests/rec/white_noise/frontal/1622536219.0725663sin-bola.wav',
-         'tests/rec/white_noise/frontal/1622536047.389629con-bola.wav']
+base_uri = "tests/rec/white_noise/"
 
-fs, x = read(files[1])
-x = np.transpose(x)
-x = x[1]
-
-x /= np.max(np.abs(x))
-
-print('fs:', fs)
-print('x.shape: ', x.shape)
-print('min: ', np.min(x))
-print('max: ', np.max(x))
+files = {
+    "frontal": {
+        "con-bola": base_uri+'frontal/1622536219.0725663sin-bola.wav',
+        "sin-bola": base_uri+'frontal/1622536047.389629con-bola.wav'},
+    "lateral": {
+        "con-bola": base_uri+'lateral/1622535890.7199836con-bola.wav',
+        "sin-bola": base_uri+'lateral/1622536401.8462512sin-bola.wav',
+    }}
 
 
 # %%
+def load_wav(file, verbose=False):
+    fs, x = read(file)
+    x = np.transpose(x)
+    x = x[1]
+
+    # x /= np.max(np.abs(x))
+
+    if verbose:
+        print('Loading: ', file)
+        print('fs:', fs)
+        print('x.shape: ', x.shape)
+        print('min: ', np.min(x))
+        print('max: ', np.max(x))
+    return x, fs
+
+
 def get_num_of_windows(len_audio, win_size, overlap):
     i = 0
     num_of_win = 0
@@ -35,16 +47,16 @@ def get_num_of_windows(len_audio, win_size, overlap):
 
 
 # %%
-def get_spectrum(win_size=2**8):
+def get_spectrum(file, win_size=2**8):
+    x, fs = load_wav(file)
+
     win_type = 'hanning'
-    overlap = 0.1
+    window = signal.windows.hann(win_size)
+    overlap = 0.5
     len_audio = x.shape[0]
-    fraction = 3
 
     i = 0
     index = 0
-
-    window = signal.windows.hann(win_size)
     spls = []
 
     while i+win_size < len_audio:
@@ -66,15 +78,64 @@ def get_spectrum(win_size=2**8):
         i += round(win_size-win_size*overlap)
 
     # Apply mean to all of the SPLs
-    spl = np.mean(spls, 0)
+    return np.mean(spls, 0), xf
 
+
+def show_freq_response(freq, spls, labels=['']):
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
 
-    ax.plot(xf, spl)
-    ax.set_xscale('log')
-    ax.set_xlim([20, 20e3])
+    for index, spl in enumerate(spls):
+        label = labels[index]
+        # ax.plot(freq, spl, ':', label=label)
+
+        spl2 = uniform_filter1d(spl, size=100)
+        ax.plot(freq, spl2, '-', label=label+' smooth')
+        ax.set_xscale('log')
+        ax.set_xlim([20, 20e3])
+        ax.legend()
+        ax.grid(True)
+
     plt.show()
 
 
-get_spectrum(win_size=2**10)
+win_size = 2**12
+# runnign average N
+N = 200
+
+fr_con, freq = get_spectrum(files['frontal']['con-bola'], win_size)
+fr_sin, _ = get_spectrum(files['frontal']['sin-bola'], win_size)
+la_con, _ = get_spectrum(files['lateral']['con-bola'], win_size)
+la_sin, _ = get_spectrum(files['lateral']['sin-bola'], win_size)
+
+fig = plt.figure()
+
+ax = fig.add_subplot(2, 1, 1)
+ax.plot(freq, fr_con, 'b-.', label='con bola')
+ax.plot(freq, fr_sin, 'm-.', label='sin bola')
+fr_con_smoth = uniform_filter1d(fr_con, size=N)
+ax.plot(freq, fr_con_smoth, 'c-', label='con bola (smooth)', linewidth=2)
+fr_sin_smoth = uniform_filter1d(fr_sin, size=N)
+ax.plot(freq, fr_sin_smoth, 'r-', label='sin bola (smooth)', linewidth=2)
+
+ax.set_xscale('log')
+ax.set_xlim([100, 20e3])
+ax.legend()
+ax.grid(True)
+ax.set_title('Frontal')
+
+ax = fig.add_subplot(2, 1, 2)
+ax.plot(freq, la_con, 'b-.', label='con bola')
+ax.plot(freq, la_sin, 'm-.', label='sin bola')
+la_con_smoth = uniform_filter1d(la_con, size=N)
+ax.plot(freq, la_con_smoth, 'c-', label='con bola (smooth)', linewidth=2)
+la_sin_smoth = uniform_filter1d(la_sin, size=N)
+ax.plot(freq, la_sin_smoth, 'r-', label='sin bola (smooth)', linewidth=2)
+
+ax.set_xscale('log')
+ax.set_xlim([100, 20e3])
+ax.legend()
+ax.grid(True)
+ax.set_title('Lateral')
+
+plt.show()
